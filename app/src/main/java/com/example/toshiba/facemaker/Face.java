db7eb8fa06ec5b44ecfc8e19527b5b8e57792f58
golder18 <golder18@up.edu>
2018-02-14 19:37:07 -0800

    Commit 5

--- app/src/main/java/com/example/toshiba/facemaker/Face.java
+++ app/src/main/java/com/example/toshiba/facemaker/Face.java
@@ -2,6 +2,7 @@ package com.example.toshiba.facemaker;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -25,6 +26,9 @@ public class Face extends SurfaceView {
     private int[] skinArray = new int[3];
     private int[] eyeArray = new int[3];
     private int[] hairArray = new int[3];
+    private Paint skin = new Paint();
+    private Paint eyes = new Paint();
+    private Paint hair = new Paint();
 
     public Face(Context context){
         super(context);
@@ -53,16 +57,18 @@ public class Face extends SurfaceView {
     @Override
     public void onDraw(Canvas canvas){
         //Initialize face with random generation of color/style
-        Log.i("FaceMaker", "onDraw called");
+        Log.i("FaceMaker", "onDraw");
         int xCenter = (canvas.getWidth())/2;
         int yCenter = (canvas.getHeight())/2;
-        Paint skin = new Paint();
+        Paint myPaint = new Paint();
         skin.setColor(skinColor);
         skin.setStrokeWidth(5.0f);
+        eyes.setColor(eyeColor);
+        hair.setColor(hairColor);
 
         //Draw selected hairstyle
         if( hairStyle == 0 ){
-            drawGirlCut(canvas);
+            drawGirlCut(canvas, xCenter, yCenter);
         }
         else if( hairStyle == 1 ){
             drawFrostedTips(canvas);
@@ -71,20 +77,28 @@ public class Face extends SurfaceView {
             drawPartyHat(canvas);
         }
 
-        Paint eyes = new Paint();
-        Paint hair = new Paint();
-        eyes.setColor(eyeColor);
-        hair.setColor(hairColor);
-
         //Paint face
-        canvas.drawCircle(xCenter, yCenter, 400.0f, skin);
-
-
+        for(int i = 0; i<100; i = i+10){
+            canvas.drawCircle(xCenter, yCenter+i, 400.0f, skin);
+        }
         //Paint eyes
+        //external citation: set sty
+        canvas.drawCircle(xCenter - 175, yCenter, 170, eyes);
+        canvas.drawCircle(xCenter + 175, yCenter, 170, eyes);
+        myPaint.setColor(Color.BLACK);
+        canvas.drawCircle(xCenter - 175, yCenter, 120, myPaint);
+        canvas.drawCircle(xCenter + 175, yCenter, 120, myPaint);
+        myPaint.setStyle(Paint.Style.STROKE);
+        myPaint.setStrokeWidth(5.0f);
+        //canvas.drawCircle(xCenter - 175, yCenter, 170, myPaint);
+        //canvas.drawCircle(xCenter + 175, yCenter, 170, myPaint);
+
         //Nose? Mouth?
+
     }
 
     public void randomize() {
+        Log.i("FaceMaker", "randomize");
         Random rand = new Random();
         for(int i = 0; i<3; i++){
             skinArray[i] = rand.nextInt(255);
@@ -101,9 +115,11 @@ public class Face extends SurfaceView {
         //E.g. Eye: red val, green val, blue val with getter methods
     }
 
-    public void drawGirlCut(Canvas canvas){
-        //Paint circle
-        //Paint rect
+    public void drawGirlCut(Canvas canvas, int xCenter, int yCenter){
+        hair.setStrokeWidth(5.0f);
+        canvas.drawCircle(xCenter, yCenter, 500, hair);
+        canvas.drawRect(xCenter-500.0f, yCenter, xCenter+500,
+                    yCenter+700, hair);
     }
 
     public void drawFrostedTips(Canvas canavas){
@@ -117,12 +133,14 @@ public class Face extends SurfaceView {
     }
 
     public void rgbToInt(){
+        Log.i("FaceMaker", "rgbToInt");
         skinColor = rgb(skinArray[0], skinArray[1], skinArray[2]);
         eyeColor = rgb(eyeArray[0], eyeArray[1], eyeArray[2]);
         hairColor = rgb(hairArray[0], hairArray[1], hairArray[2]);
     }
 
     public void update(){
+        Log.i("FaceMaker", "update");
         //external citation
         //https://stackoverflow.com/questions/18607335/how-to-update-a-surfaceviewLog.i("Face", "Update Face");
         invalidate();
@@ -130,6 +148,7 @@ public class Face extends SurfaceView {
 
     //Getters and setters
     public void setColorVal(int newValue, int color, String faceFeat){
+        Log.i("FaceMaker", "setColorVal");
         if(faceFeat.equals("Hair")){
             hairArray[color] = newValue;
         }
