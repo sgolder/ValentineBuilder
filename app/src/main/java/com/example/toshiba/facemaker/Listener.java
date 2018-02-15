1ea38ed9f17ee2b7ba54866fe813719c423cfdd2
golder18 <golder18@up.edu>
2018-02-14 19:37:45 -0800

    Commit 6

--- app/src/main/java/com/example/toshiba/facemaker/Listener.java
+++ app/src/main/java/com/example/toshiba/facemaker/Listener.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 
 /**
  * Created by Sarah Golder on 2/12/2018.
@@ -23,19 +24,17 @@ public class Listener implements View.OnClickListener,
     private RadioButton eyesRad = null;
     private RadioButton skinRad = null;
     private Face face = null;
-    //private Button randButton = null;
-    String faceFeat;
+    private Spinner hairSpin;
+    private Button randButton = null;
+    String faceFeat = "";
 
     @Override
     public void onClick(View v) {
         //Randomize button listener
-        Button cur = (Button)v; //recently clicked button
-        String curLabel = (String)cur.getText();
+        Log.i("Listener", "onClick");
         int[] colorArray = new int[3];
-        Log.i("Listener", "onClick curLabel: "+curLabel);
 
-        if(curLabel.equalsIgnoreCase("Generate Random Face")){
-            //randButton = cur;
+        if(v.equals(randButton)){
             face.randomize();
         }
 
@@ -46,13 +45,23 @@ public class Listener implements View.OnClickListener,
             updateSeekBars(colorArray);
             faceFeat = "Skin";
         }
-        //SeekBar setProgress based on selection
-
+        else if( eyesRad.isChecked() ){
+            colorArray = face.getEyeArray();
+            updateSeekBars(colorArray);
+            faceFeat = "Eye";
+        }
+        else if( hairRad.isChecked() ){
+            colorArray = face.getHairArray();
+            updateSeekBars(colorArray);
+            faceFeat = "Hair";
+        }
         face.update();
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        Log.i("Listener", "onProgressChanged");
+        if( !fromUser ){ return; }
         SeekBar cur = (SeekBar) seekBar;
         int curId = cur.getId();
         //Insert external citation
@@ -78,16 +87,25 @@ public class Listener implements View.OnClickListener,
     }
 
     public void addViews(SurfaceView initface, SeekBar[] seekBars,
-                         RadioButton[] radGroup){
+                         RadioButton[] radGroup, Spinner spinner,
+                         Button rand){
+        Log.i("Listener", "addViews");
         face = (Face)initface;
+
         redSeek = seekBars[0];
         greenSeek = seekBars[1];
         blueSeek = seekBars[2];
+
+        hairRad = radGroup[0];
+        eyesRad = radGroup[1];
         skinRad = radGroup[2];
 
+        randButton = rand;
+        hairSpin = spinner;
     }
 
     public void updateSeekBars(int[] colorArray){
+        Log.i("Listener", "updateSeekBars");
         redSeek.setProgress(colorArray[0]);
         greenSeek.setProgress(colorArray[1]);
         blueSeek.setProgress(colorArray[2]);
@@ -103,9 +121,8 @@ public class Listener implements View.OnClickListener,
         //Will not use
     }
 
-
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-        //Nothing will display
+        //Will not use
     }
 }
